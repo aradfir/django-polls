eebedc2e315ee477e9a0b74238c399115112022b
Arad Firouzkouhi <aradfir@gmail.com>
2022-08-02 12:26:23 +0430

    register template and view

--- polls/views.py
+++ polls/views.py
@@ -117,6 +117,28 @@ def do_login(request):
         return render(request, 'login_response.html')
 
 
+from .forms import RegisterForm
+from django.contrib.auth.models import User
+
+
+@require_http_methods(['POST', 'GET'])
+def register_form(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(username=form.cleaned_data.get('user_name'),
+                                                email=form.cleaned_data.get('email'),
+                                                password=form.cleaned_data.get('password'))
+            new_user.first_name = form.cleaned_data.get('first_name')
+            new_user.last_name = form.cleaned_data.get('last_name')
+            new_user.save()
+            login(request, new_user)
+            return HttpResponse("You have registered successfully!")
+    else:
+        form = RegisterForm()
+    return render(request, 'register.html', {'form': form})
+
+
 def log_out(request):
     logout(request)
     return redirect(reverse('login_form'))
